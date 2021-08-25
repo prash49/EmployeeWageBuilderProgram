ab7c42a067adc4c2ace7f21f07b221f15a703232
Prashanth <imprash4u@gmail.com>
2021-08-25 09:36:58 +0530

    Added: Functionality To check Present or Absent

--- src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
@@ -1,9 +1,15 @@
 package com.bridgelabz;
 
-public class EmployeeWageBuilderProgram
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welocome To Employee Wage Builder" );
+public class EmployeeWageBuilderProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage");
+
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("employee is present");
+        } else {
+            System.out.println("employee is absent");
+        }
     }
 }
