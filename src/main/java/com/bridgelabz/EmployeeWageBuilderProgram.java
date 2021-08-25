6aee4926f2e49d4e0b5b8998c618cdd00bb89d6a
Prashanth <imprash4u@gmail.com>
2021-08-25 09:56:24 +0530

    Added: Functionality to calculate wage

--- src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
@@ -1,15 +1,22 @@
 package com.bridgelabz;
 
-public class EmployeeWageBuilderProgram {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage");
-
+public class EmployeeWageBuilderProgram
+{
+    public static void main( String[] args ){
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
             System.out.println("employee is present");
+            empHrs=8;
         } else {
             System.out.println("employee is absent");
+            empHrs=0;
         }
+        int empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage " + empWage);
     }
+
 }
