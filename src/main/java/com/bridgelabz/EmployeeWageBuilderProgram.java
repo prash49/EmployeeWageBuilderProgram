a506d1f06e1bee313751db5c418f2d5a0c1bec0d
Prashanth <imprash4u@gmail.com>
2021-08-25 09:21:59 +0530

    Added:Welcome Message

---
+++ src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class EmployeeWageBuilderProgram
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welocome To Employee Wage Builder" );
+    }
+}
