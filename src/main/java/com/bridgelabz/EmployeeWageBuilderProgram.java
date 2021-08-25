ac2a293bffde525aa2a00e13c7bbede9e068a7a9
Prashanth <imprash4u@gmail.com>
2021-08-25 12:09:42 +0530

    Added:Total Employeewage calculation for 20days

--- src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
@@ -10,20 +10,28 @@ public class EmployeeWageBuilderProgram {
         // variables
         int empHrs = 0;
         int empWage = 0;
-        //Getting Random Number by Random method
-        //Math.floor is used to Round off the value
-        int empCheck = (int)(Math.floor(Math.random() * 10) % 3);
-        switch ( empCheck) {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+        int numberOfWorkingDays = 20;
+        int totalWage = 0;
+        for (int i = 1; i <= numberOfWorkingDays; i++) {
+            //Getting Random Number by Random method
+            //Math.floor is used to Round off the value
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            //Daily_wage checking based on emp_attendance
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("day" + i + "Employee Wage=" + empWage);
+            //adding daily wage into total
+            totalWage += empWage;
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage=" + empWage);
+        System.out.println("Total wage of Employee for 20days:" + totalWage);
     }
 }
