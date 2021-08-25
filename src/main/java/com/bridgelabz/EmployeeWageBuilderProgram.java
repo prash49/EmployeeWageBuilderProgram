c4496f7a3a1bdf73c4d8cc10c9a865d30179e008
Prashanth <imprash4u@gmail.com>
2021-08-25 11:52:38 +0530

    Added:calWage using Switch case

--- src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilderProgram.java
@@ -1,24 +1,29 @@
 package com.bridgelabz;
 
-public class EmployeeWageBuilderProgram
-{
-    public static void main( String[] args ){
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+public class EmployeeWageBuilderProgram {
+    //Constants
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
+    public static void main(String[] args) {
+        // variables
         int empHrs = 0;
-
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("employee is present");
-            empHrs = 8;
-        } else if(empCheck == IS_PART_TIME) {
-            System.out.println("employee is absent");
-            empHrs = 4;
-        } else
-            empHrs = 0;
-        int empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage " + empWage);
+        int empWage = 0;
+        //Getting Random Number by Random method
+        //Math.floor is used to Round off the value
+        int empCheck = (int)(Math.floor(Math.random() * 10) % 3);
+        switch ( empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage=" + empWage);
     }
-
 }
